1fa1167823c5e52e639f8ddc1e80359e3479eb1a
devzz-web <65944653+devzz-web@users.noreply.github.com>
2021-10-01 00:36:52 +0530

    Update hcf.cpp

--- hcf.cpp
+++ hcf.cpp
@@ -15,7 +15,27 @@
     }
     //body of the function
     int findGCD(int first, int second)
-    {
+    { 
+           if(first == 0)
+            {    
+            return second;
+            }
+            if(second == 0)
+            {    
+            return first;
+            }
+            // both numbers are equal
+            if(first == second)
+            {
+            return first;
+            }
+            // first is greater
+            else if(first > second)
+            {
+            return findGCD(first – second, second);
+            }
+            return findGCD(first, second – first);
+
             // 0 is divisible by every number
             
     }
