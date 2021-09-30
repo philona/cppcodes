e82b8fdcd0e3c22adbd95e11826b39b540069e7e
codergeekzz <91679371+codergeekzz@users.noreply.github.com>
2021-09-30 19:32:45 +0530

    added program

---
+++ binarytodecimal.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+    #include <math.h>
+    using namespace std;
+    //function to convert binary to decimal
+    int convert(long n)
+    {
+        int i = 0,decimal= 0;
+        //converting binary to decimal
+        while (n!=0)
+        {
+           
+        }
+        return decimal;
+    }
+    //main program
+    int main()
+    {
+        long binary;
+        cout << “Enter binary number: “;
+        cin >> binary;  
+        cout << binary << ” in binary = “ << convert(binary) << ” in decimal”;
+        return 0;
+    }
