ba7029af5aa6d91800b7bcea9171bcf87baa0f38
codergeekzz <91679371+codergeekzz@users.noreply.github.com>
2021-09-30 19:30:48 +0530

    added program

---
+++ sumofdigits.cpp
@@ -0,0 +1,18 @@
+#include
+    using namespace std;
+    int main()
+    {
+        int num,sum=0;
+        cout<<“Enter any num : “;
+        //user input
+        cin>>num;
+        //loop to find sum of digits
+        do
+        {
+                sum+=num%10;
+                num=num/10;
+        }while(num!=0);
+        //output
+        cout<<“\nSum of digits in given integer is: “<<sum;
+        return 0;
+    }
