135ebb845a6a38a7e0e22b2bd01dfe40fab6f868
codergeekzz <91679371+codergeekzz@users.noreply.github.com>
2021-09-30 19:31:47 +0530

    program added

---
+++ hcf.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+    using namespace std;
+    // Recursive function declaration
+    int findGCD(int, int);
+    // main program
+    int main()
+    {
+            int first, second;
+            cout<<“Enter First Number: “;
+            cin>>first;
+            cout<<“Enter second Number: “;
+            cin>>second;
+            cout<<“GCD of “<<first<<” and “<<second<<” is “<<findGCD(first,second);
+            return 0;
+    }
+    //body of the function
+    int findGCD(int first, int second)
+    {
+            // 0 is divisible by every number
+            
+    }
