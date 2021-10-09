95cfb977ca032e74c3056f1a71dd3c2bc10b6ce6
shubhibishnoi <72508847+shubhibishnoi@users.noreply.github.com>
2021-10-09 12:57:03 +0530

    KanpSack top-down method added

--- knapsack_0-1.cpp
+++ knapsack_0-1.cpp
@@ -11,36 +11,105 @@ You cannot break an item, either pick the complete item or
 don’t pick it (0-1 property).
 */
 
-
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int knapsack(int W, vector<int> &wt, vector<int> &val, int n){
-    vector<vector<int>> K(n+1,vector<int>(W+1));
-    for(int i=0;i<=n;i++){
-        for(int w=0;w<=W;w++){
-            if(i==0||w==0){
-                K[i][w]=0;
+int knapsack(int W, vector<int> &wt, vector<int> &val, int n)
+{
+    vector<vector<int>> K(n + 1, vector<int>(W + 1));
+    for (int i = 0; i <= n; i++)
+    {
+        for (int w = 0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+            {
+                K[i][w] = 0;
             }
-            else if(wt[i-1]<=w){
-                K[i][w]=max(val[i-1]+K[i-1][w-wt[i-1]],K[i-1][w]);
+            else if (wt[i - 1] <= w)
+            {
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
             }
-            else{
-                K[i][w]=K[i-1][w];
+            else
+            {
+                K[i][w] = K[i - 1][w];
             }
         }
     }
     return K[n][W];
 }
 
-
-int main(){
-    vector<int> val = { 60, 100, 120 };
-    vector<int> wt = { 10, 20, 30 };
+int main()
+{
+    vector<int> val = {60, 100, 120};
+    vector<int> wt = {10, 20, 30};
     int W = 50;
     int n = sizeof(val) / sizeof(val[0]);
-     
+
     cout << knapsack(W, wt, val, n);
+    return 0;
+}
+
+// memoisation or top-down approach
+
+// Here is the top-down approach of
+// dynamic programming
+#include <bits/stdc++.h>
+using namespace std;
+
+int knapSackRec(int W, int wt[],
+                int val[], int i,
+                int **dp)
+{
+    // base condition
+    if (i < 0)
+        return 0;
+    if (dp[i][W] != -1)
+        return dp[i][W];
+
+    if (wt[i] > W)
+    {
+
+        dp[i][W] = knapSackRec(W, wt,
+                               val, i - 1,
+                               dp);
+        return dp[i][W];
+    }
+    else
+    {
+
+        dp[i][W] = max(val[i] + knapSackRec(W - wt[i],
+                                            wt, val,
+                                            i - 1, dp),
+                       knapSackRec(W, wt, val,
+                                   i - 1, dp));
+
+        return dp[i][W];
+    }
+}
+
+int knapSack(int W, int wt[], int val[], int n)
+{
+    //craeting a 2D array
+    int **dp;
+    dp = new int *[n];
+
+    for (int i = 0; i < n; i++)
+        dp[i] = new int[W + 1];
+
+    //initialsising arrar with -1
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < W + 1; j++)
+            dp[i][j] = -1;
+    return knapSackRec(W, wt, val, n - 1, dp);
+}
+
+// Driver Code
+int main()
+{
+    int val[] = {60, 100, 120};
+    int wt[] = {10, 20, 30};
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    cout << knapSack(W, wt, val, n);
     return 0;
 }
