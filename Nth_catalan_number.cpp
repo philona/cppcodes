6b2040a94016766195129bfb97bba4c5aa7841f8
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-10-28 19:43:33 +0530

    Update Nth_catalan_number.cpp

--- Nth_catalan_number.cpp
+++ Nth_catalan_number.cpp
@@ -1,3 +1,12 @@
+/* Catalan numbers are a sequence of natural numbers that occurs in many interesting counting problems like following.
+
+1.Count the number of expressions containing n pairs of parentheses which are correctly matched. For n = 3, possible expressions are ((())), ()(()), ()()(), (())(), (()()).
+2.Count the number of possible Binary Search Trees with n keys (See this)
+3.Count the number of full binary trees (A rooted binary tree is full if every vertex has either two children or no children) with n+1 leaves.
+4.Given a number n, return the number of ways you can draw n chords in a circle with 2 x n points such that no 2 chords intersect.
+
+The first few Catalan numbers for n = 0, 1, 2, 3, … are 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, …  */
+
 #include <iostream>
 using namespace std;
 
@@ -25,5 +34,10 @@ int main()
 		cout << catalan(i) << " ";
 	return 0;
 }
+/* O/P 
+1 1 2 5 14 42 132 429 1430 4862 
+*/
+
+
 
 
